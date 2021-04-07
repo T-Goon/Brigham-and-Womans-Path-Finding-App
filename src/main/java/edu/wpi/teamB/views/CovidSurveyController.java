3aafce58d138e136308a3db5bb7fcb3fc62a2cd4
brian <bfrancis@wpi.edu>
2021-04-07 01:19:25 -0400

    Begining of covid survey

---
+++ src/main/java/edu/wpi/teamB/views/CovidSurveyController.java
@@ -0,0 +1,16 @@
+package edu.wpi.teamB.views;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+import java.io.IOException;
+
+public class CovidSurveyController {
+    @FXML
+    private JFXButton submitBtn;
+
+    @FXML
+    void handleButtonAction(ActionEvent e) throws IOException {}
+}
