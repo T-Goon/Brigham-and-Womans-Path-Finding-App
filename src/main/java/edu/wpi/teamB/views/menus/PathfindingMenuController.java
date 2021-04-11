d7a093f4b4e475a5bc53e20c9b8a5162c8c6fdda
Timothy Goon <t.kgoonx@gmail.com>
2021-04-11 16:49:56 -0400

    Can pan and zoom map.

--- src/main/java/edu/wpi/teamB/views/menus/PathfindingMenuController.java
+++ src/main/java/edu/wpi/teamB/views/menus/PathfindingMenuController.java
@@ -2,12 +2,16 @@ package edu.wpi.teamB.views.menus;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.pannable.PannablePane;
 import edu.wpi.teamB.pathfinding.Node;
 import edu.wpi.teamB.pathfinding.Read;
 import edu.wpi.teamB.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.input.ZoomEvent;
 
 import java.io.IOException;
 import java.net.URL;
@@ -23,17 +27,26 @@ public class PathfindingMenuController implements Initializable {
     private JFXComboBox<String> endLocComboBox;
 
     @FXML
+    private ImageView map;
+
+    @FXML
     private JFXButton findPathBtn;
 
     @FXML
+    private JFXButton zoomInBtn;
+
+    @FXML
+    private JFXButton zoomOutBtn;
+
+    @FXML
     private JFXButton backBtn;
+
+    private static final double zoomAmount = 400;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         HashMap<String, Node> locations = Read.parseCSVNodes();
-
-        System.out.println(locations.values());
 
         for (Node n : locations.values()){
             startLocComboBox.getItems().add(n.getLongName());
@@ -49,10 +62,33 @@ public class PathfindingMenuController implements Initializable {
             case "findPathBtn":
                 // TODO pathfinding stuff
                 break;
+            case "zoomInBtn":
+                zoomMap(PathfindingMenuController.zoomAmount);
+                break;
+            case "zoomOutBtn":
+                zoomMap(-PathfindingMenuController.zoomAmount);
+                break;
             case "backBtn":
                 SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/menus/patientDirectoryMenu.fxml");
-            break;
+                break;
         }
+    }
+
+    @FXML
+    private void zoom(ZoomEvent e){
+        zoomMap(PathfindingMenuController.zoomAmount);
+    }
+
+    /**
+     * Zooms the map byt the value.
+     * @param value Value to zoom in or out.
+     */
+    private void zoomMap(double value){
+        int w = map.fitWidthProperty().intValue();
+        int h = map.fitHeightProperty().intValue();
+
+        map.setFitWidth(w+value);
+        map.setFitHeight(h+value);
     }
 
     /**
