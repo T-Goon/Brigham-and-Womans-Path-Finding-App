7d88427af3be3f1cca64a65ff46d77ed2cd5ade0
Alex Bolduc <aebolduc@wpi.edu>
2021-04-11 19:59:06 -0400

    Fix checkbox hitboxes and separate radio buttons

--- src/main/java/edu/wpi/teamB/views/secondaryForms/CovidSurveyController.java
+++ src/main/java/edu/wpi/teamB/views/secondaryForms/CovidSurveyController.java
@@ -3,15 +3,19 @@ package edu.wpi.teamB.views.secondaryForms;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 import edu.wpi.teamB.App;
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.ToggleGroup;
 import javafx.fxml.Initializable;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class CovidSurveyController implements Initializable {
@@ -47,6 +51,7 @@ public class CovidSurveyController implements Initializable {
         btnTestNo.setToggleGroup(testGroup);
 
     }
+
 
     @FXML
     void handleButtonAction(ActionEvent e) {
