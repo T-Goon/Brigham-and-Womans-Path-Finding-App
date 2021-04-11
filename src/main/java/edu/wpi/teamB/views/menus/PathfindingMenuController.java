87e506849c0db35d30f4e0670e95d7b8cb9a5f46
Timothy Goon <t.kgoonx@gmail.com>
2021-04-11 19:21:29 -0400

    Zoomable, Panneble map. Added functionality to draw nodes and edges on the map

--- src/main/java/edu/wpi/teamB/views/menus/PathfindingMenuController.java
+++ src/main/java/edu/wpi/teamB/views/menus/PathfindingMenuController.java
@@ -2,17 +2,18 @@ package edu.wpi.teamB.views.menus;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.pannable.PannablePane;
+
 import edu.wpi.teamB.pathfinding.Node;
 import edu.wpi.teamB.pathfinding.Read;
 import edu.wpi.teamB.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.input.ZoomEvent;
-
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
@@ -27,31 +28,46 @@ public class PathfindingMenuController implements Initializable {
     private JFXComboBox<String> endLocComboBox;
 
     @FXML
+    private AnchorPane mapHolder;
+
+    @FXML
     private ImageView map;
 
     @FXML
-    private JFXButton findPathBtn;
+    private JFXButton btnFindPath;
 
     @FXML
-    private JFXButton zoomInBtn;
+    private JFXButton btnZoomIn;
 
     @FXML
-    private JFXButton zoomOutBtn;
+    private JFXButton btnZoomOut;
 
     @FXML
-    private JFXButton backBtn;
+    private JFXButton btnPlaceDot;// TODO This is just and example. Remove it later.
 
-    private static final double zoomAmount = 400;
+    @FXML
+    private JFXButton btnBack;
+
+    private static final double zoomAmount = .2;
+    private static final double zoomMin = .1;
+    private static final double zoomMax = 10;
+    private static final double coordinateScale = 3.33333333333333333333333333333333333333333;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         HashMap<String, Node> locations = Read.parseCSVNodes();
 
-        for (Node n : locations.values()){
-            startLocComboBox.getItems().add(n.getLongName());
-            endLocComboBox.getItems().add(n.getLongName());
+        // Populate the combo boxes with locations
+        try{
+            for (Node n : locations.values()){
+                startLocComboBox.getItems().add(n.getLongName());
+                endLocComboBox.getItems().add(n.getLongName());
+            }
+        } catch (NullPointerException e){
+            e.printStackTrace();
         }
+
     }
 
     @FXML
@@ -59,36 +75,98 @@ public class PathfindingMenuController implements Initializable {
         JFXButton b = (JFXButton) e.getSource();
 
         switch (b.getId()){
-            case "findPathBtn":
+            case "btnFindPath":
                 // TODO pathfinding stuff
                 break;
-            case "zoomInBtn":
-                zoomMap(PathfindingMenuController.zoomAmount);
+            case "btnZoomIn":
+                zoomMap(true);
                 break;
-            case "zoomOutBtn":
-                zoomMap(-PathfindingMenuController.zoomAmount);
+            case "btnZoomOut":
+                zoomMap(false);
                 break;
-            case "backBtn":
+            case "btnPlaceDot":
+                // TODO This is just and example. Remove it later.
+
+                placeEdge(2077, 2031, 56, 2078);
+
+                placeNode(2077, 2031);
+
+                placeNode(56, 2078);
+
+                break;
+            case "btnBack":
                 SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/menus/patientDirectoryMenu.fxml");
                 break;
         }
-    }
-
-    @FXML
-    private void zoom(ZoomEvent e){
-        zoomMap(PathfindingMenuController.zoomAmount);
     }
 
     /**
      * Zooms the map byt the value.
      * @param value Value to zoom in or out.
      */
-    private void zoomMap(double value){
-        int w = map.fitWidthProperty().intValue();
-        int h = map.fitHeightProperty().intValue();
+    private void zoomMap(boolean value){
+        if(mapHolder.getScaleX()*(1+zoomAmount) < PathfindingMenuController.zoomMax &&
+                mapHolder.getScaleY()*(1+zoomAmount) < PathfindingMenuController.zoomMax &&
+                value){
+            mapHolder.setScaleX(mapHolder.getScaleX()*(1+zoomAmount));
+            mapHolder.setScaleY(mapHolder.getScaleX()*(1+zoomAmount));
+        }
 
-        map.setFitWidth(w+value);
-        map.setFitHeight(h+value);
+        if(mapHolder.getScaleX()*(1-zoomAmount) > PathfindingMenuController.zoomMin &&
+                mapHolder.getScaleY()*(1-zoomAmount) > PathfindingMenuController.zoomMin &&
+                !value){
+            mapHolder.setScaleX(mapHolder.getScaleX()*(1-zoomAmount));
+            mapHolder.setScaleY(mapHolder.getScaleX()*(1-zoomAmount));
+        }
+    }
+
+    /**
+     * Places an image for a node on the map at the given pixel coordinates.
+     *
+     * IMPORTANT!!!: ALWAYS PLACE EDGES BEFORE NODES.
+     *
+     * @param x x coordinates of node in pixels
+     * @param y y coordinates of node in pixels
+     */
+    public void placeNode(int x, int y) {
+        try {
+            Circle c = FXMLLoader.load(getClass().getResource("/edu/wpi/teamB/views/misc/node.fxml"));
+
+            c.setCenterX(x/PathfindingMenuController.coordinateScale);
+            c.setCenterY(y/PathfindingMenuController.coordinateScale);
+
+            mapHolder.getChildren().add(c);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Draws an edge between 2 points on the map.
+     *
+     * IMPORTANT!!!: ALWAYS PLACE EDGES BEFORE NODES.
+     *
+     * @param xStart x start coordinate in pixels
+     * @param yStart y start coordinate in pixels
+     * @param xEnd x end coordinate in pixels
+     * @param yEnd y end coordinate in pixels
+     */
+    public void placeEdge(int xStart, int yStart, int xEnd, int yEnd){
+        try {
+            Line l = FXMLLoader.load(getClass().getResource("/edu/wpi/teamB/views/misc/edge.fxml"));
+
+            l.setStartX(xStart/PathfindingMenuController.coordinateScale);
+            l.setStartY(yStart/PathfindingMenuController.coordinateScale);
+
+            l.setEndX(xEnd/PathfindingMenuController.coordinateScale);
+            l.setEndY(yEnd/PathfindingMenuController.coordinateScale);
+
+            mapHolder.getChildren().add(l);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
