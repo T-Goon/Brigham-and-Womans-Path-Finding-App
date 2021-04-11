603370a80f5ea0c7f21d534d3fe04a288a9c22d7
emperry <emperry@wpi.edu>
2021-04-11 11:39:25 -0400

    make the button work

---
+++ src/main/java/edu/wpi/teamB/views/FormSubmittedController.java
@@ -0,0 +1,32 @@
+package edu.wpi.teamB.views;
+
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.teamB.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class FormSubmittedController {
+
+    @FXML
+    private JFXButton btnReturn;
+
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+
+        Button btn = (Button) e.getSource();
+
+        if (btn == btnReturn) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamB/views/serviceRequestMenu.fxml"));
+                App.getPrimaryStage().getScene().setRoot(root);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
