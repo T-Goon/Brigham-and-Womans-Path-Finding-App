4d307d0e3f15fece930a1f46ae39115ba0319d98
smeragora <smeragora@yahoo.com>
2021-04-11 15:07:37 -0400

    Fixed dist formula

--- src/test/java/edu/wpi/teamB/PathfindingTests.java
+++ src/test/java/edu/wpi/teamB/PathfindingTests.java
@@ -3,16 +3,14 @@ package edu.wpi.teamB;
 import edu.wpi.teamB.pathfinding.Graph;
 import edu.wpi.teamB.pathfinding.Node;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PathfindingTests {
 
+public class PathfindingTests {
     @Test
-    public void hashMapTest() {
+    public void adjNodes() {
         //Testing only one adj node
         List<Node> adjNodes = new ArrayList<>();
         Node bWALK00501 = new Node("bWALK00501", 1872, 1965, 1, "Parking", "WALK", "Vining Street Walkway", "ViningWalk");
@@ -37,6 +35,16 @@ public class PathfindingTests {
         adjNodes3.add(bEXIT00201);
 
         assertEquals(adjNodes3, Graph.findAdjNodes("bWALK00401"));
+
+    }
+
+    @Test
+    public void testDist(){
+        Node start =  new Node("bWALK00301",3,0,1,"Parking","WALK","Francis Street Vining Street Intersection","FrancisViningInt");
+        Node end = new Node("bEXIT00101",0,4,1,"Parking","EXIT","75 Francis Lobby Entrance","FrancisLobbyEnt");
+
+        assertEquals(5.0, Graph.dist(start, end));
+
 
     }
 
