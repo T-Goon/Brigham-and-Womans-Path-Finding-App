73206aa3812e673c4671a198dd5a70ca94f44447
mdsunray34 <60022435+mdsunray34@users.noreply.github.com>
2021-04-13 11:20:37 -0400

    Changed scene switch after adding a node

--- src/main/java/edu/wpi/teamB/views/AddNodeMenuController.java
+++ src/main/java/edu/wpi/teamB/views/AddNodeMenuController.java
@@ -101,7 +101,7 @@ public class AddNodeMenuController implements Initializable {
                 int aYCoord = Integer.parseInt(yCoord.getText());
                 Node aNode = new Node(aNodeId, aXCoord, aYCoord, aFloor, aBuilding, aNodeType, aLongName, aShortName);
                 DatabaseHandler.getDatabaseHandler("main.db").addNode(aNode);
-                SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/menus/editorIntermediateMenu.fxml");
+                SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/menus/nodesEditorMenu.fxml");
                 break;
         }
     }
