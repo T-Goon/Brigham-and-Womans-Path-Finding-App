66bbf0510351c003d874dba106a6103d3a0462f7
brian <bfrancis@wpi.edu>
2021-04-12 21:23:31 -0400

    Created add node menu, adds node to database?, doesn't leave page

--- src/main/java/edu/wpi/teamB/views/addNodeMenuController.java
+++ src/main/java/edu/wpi/teamB/views/addNodeMenuController.java
@@ -2,10 +2,14 @@ package edu.wpi.teamB.views;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
+import edu.wpi.teamB.database.DatabaseHandler;
+import edu.wpi.teamB.entities.Node;
 import edu.wpi.teamB.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 import java.io.IOException;
@@ -23,6 +27,22 @@ public class addNodeMenuController implements Initializable {
     private JFXRadioButton restricted;
     @FXML
     private ToggleGroup areaGroup;
+    @FXML
+    private TextField nodeID;
+    @FXML
+    private TextField xCoord;
+    @FXML
+    private TextField yCoord;
+    @FXML
+    private TextField floor;
+    @FXML
+    private TextField building;
+    @FXML
+    private TextField nodeType;
+    @FXML
+    private TextField longName;
+    @FXML
+    private TextField shortName;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -39,7 +59,17 @@ public class addNodeMenuController implements Initializable {
                 SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/menus/nodesEditorMenu.fxml");
                 break;
             case "btnAddNode":
-
+                String aNodeId = nodeID.getText();
+                int aFloor = Integer.parseInt(floor.getText());
+                String aBuilding = building.getText();
+                String aNodeType = nodeType.getText();
+                String aLongName = longName.getText();
+                String aShortName = shortName.getText();
+                int aXCoord = Integer.parseInt(xCoord.getText());
+                int aYCoord = Integer.parseInt(yCoord.getText());
+                Node aNode = new Node(aNodeId,aXCoord,aYCoord, aFloor, aBuilding,aNodeType, aLongName,aShortName);
+                DatabaseHandler.getDatabaseHandler("main.db").addNode(aNode);
+                SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/menus/nodesEditorMenu.fxml");
                 break;
         }
     }
