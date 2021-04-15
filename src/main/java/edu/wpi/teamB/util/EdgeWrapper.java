848047a11e43d66725d7a8f1a6b21e5f7c340c93
Jonathan Metcalf <jonathan.e.metcalf@gmail.com>
2021-04-14 22:00:22 -0400

    fix to editing nodes/edges

--- src/main/java/edu/wpi/teamB/util/EdgeWrapper.java
+++ src/main/java/edu/wpi/teamB/util/EdgeWrapper.java
@@ -42,7 +42,7 @@ public class EdgeWrapper {
                 Stage stage = App.getPrimaryStage();
                 stage.setUserData(e);
                 try {
-                    SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/mapeditor/edges/editEdgeMenu.fxml");
+                    SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/mapEditor/edges/editEdgeMenu.fxml");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -55,12 +55,9 @@ public class EdgeWrapper {
         JFXButton btnDel = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/edu/wpi/teamB/views/misc/nodeEdgeDelBtn.fxml")));
         btnDel.setId(e.getEdgeID() + "DelBtn");
 
-        btnDel.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                DatabaseHandler.getDatabaseHandler("main.db").removeEdge(e.getEdgeID());
-                parentTable.getItems().removeIf( (Object o) -> ((EdgeWrapper) o).id == id);
-            }
+        btnDel.setOnAction(event -> {
+            DatabaseHandler.getDatabaseHandler("main.db").removeEdge(e.getEdgeID());
+            parentTable.getItems().removeIf( (Object o) -> ((EdgeWrapper) o).id == id);
         });
 
         this.btnDel = btnDel;
