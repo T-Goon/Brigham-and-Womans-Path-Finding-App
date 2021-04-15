5827a1e93630f80ea54f58dbd4482027238bd761
mdsunray34 <60022435+mdsunray34@users.noreply.github.com>
2021-04-15 00:28:04 -0400

    Created login pages

---
+++ src/main/java/edu/wpi/teamB/views/loginPages/StaffLoginController.java
@@ -0,0 +1,35 @@
+package edu.wpi.teamB.views.loginPages;
+
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.teamB.util.SceneSwitcher;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class StaffLoginController {
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private JFXButton btnBack;
+
+    @FXML
+    private JFXButton btnEmergency;
+
+    @FXML
+    public void handleButtonAction(ActionEvent actionEvent) throws IOException {
+        JFXButton btn = (JFXButton) actionEvent.getSource();
+
+        switch (btn.getId()) {
+            case "btnLogin":
+                break;
+            case "btnBack":
+                SceneSwitcher.switchScene(getClass(), "/edu/wpi/teamB/views/loginPages/loginOptions.fxml");
+                break;
+            case "btnEmergency":
+                // Not implemented
+                break;
+        }
+    }
+}
