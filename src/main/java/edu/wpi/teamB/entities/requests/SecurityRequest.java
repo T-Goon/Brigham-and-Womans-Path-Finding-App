4b413dc3073de45e54b118d3fd4d38726ce8704f
Jonathan Metcalf <jonathan.e.metcalf@gmail.com>
2021-04-16 17:49:36 -0400

    Added more entities classes

--- src/main/java/edu/wpi/teamB/entities/requests/SecurityRequest.java
+++ src/main/java/edu/wpi/teamB/entities/requests/SecurityRequest.java
@@ -1,4 +1,16 @@
 package edu.wpi.teamB.entities.requests;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SecurityRequest extends Request {
+
+    private int urgency;
+
+    public SecurityRequest(int urgency, String requestID, String requestType, String employeeName, String description) {
+        super(requestID, requestType, employeeName, description);
+        this.urgency = urgency;
+    }
 }
