3e6da2859a6b3a0077d96e8da538ed551a1defc3
Jonathan Metcalf <jonathan.e.metcalf@gmail.com>
2021-04-17 16:37:42 -0400

    fixes to case manager/social worker

---
+++ src/main/java/edu/wpi/teamB/views/requestForms/CaseManagerRequestFormController.java
@@ -0,0 +1,4 @@
+package edu.wpi.teamB.views.requestForms;
+
+public class CaseManagerRequestFormController extends DefaultServiceRequestFormController {
+}
---
+++ src/main/java/edu/wpi/teamB/views/requestForms/SocialWorkerRequestFormController.java
@@ -0,0 +1,4 @@
+package edu.wpi.teamB.views.requestForms;
+
+public class SocialWorkerRequestFormController extends DefaultServiceRequestFormController {
+}
