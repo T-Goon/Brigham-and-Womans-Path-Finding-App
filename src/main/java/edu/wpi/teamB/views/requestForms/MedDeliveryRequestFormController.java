a596435b03a16da32d0d4748bac572a213f1a1b5
Timothy Goon <t.kgoonx@gmail.com>
2021-04-19 19:46:44 -0400

    Med Delivery request form validates button.

--- src/main/java/edu/wpi/teamB/views/requestForms/MedDeliveryRequestFormController.java
+++ src/main/java/edu/wpi/teamB/views/requestForms/MedDeliveryRequestFormController.java
@@ -4,6 +4,9 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class MedDeliveryRequestFormController extends DefaultServiceRequestFormController {
 
     @FXML
@@ -17,4 +20,18 @@ public class MedDeliveryRequestFormController extends DefaultServiceRequestFormC
 
     @FXML
     private JFXTextArea reason;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
+        btnSubmit.setDisable(true);
+    }
+
+    @FXML
+    private void validateButton(){
+        btnSubmit.setDisable(
+                name.getText().isEmpty() || roomNum.getText().isEmpty() || medName.getText().isEmpty() ||
+                    reason.getText().isEmpty()
+        );
+    }
 }
