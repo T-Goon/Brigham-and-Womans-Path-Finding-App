88872eb05772fca646e732ef5db9d31e5366f353
Timothy Goon <t.kgoonx@gmail.com>
2021-04-20 19:29:41 -0400

    Fix emergency form.

--- src/main/java/edu/wpi/teamB/views/requestForms/EmergencyFormController.java
+++ src/main/java/edu/wpi/teamB/views/requestForms/EmergencyFormController.java
@@ -4,12 +4,19 @@ import com.jfoenix.controls.*;
 import edu.wpi.teamB.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class EmergencyFormController {
+public class EmergencyFormController implements Initializable {
 
     @FXML
     private JFXTextField wing;
+
+    @FXML
+    private JFXTextField roomNum;
 
     @FXML
     private JFXCheckBox medicalEmergency;
@@ -29,17 +36,31 @@ public class EmergencyFormController {
     @FXML
     private JFXButton btnHelp;
 
-    public void handleButtonAction(ActionEvent actionEvent) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btnSubmit.setDisable(true);
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent actionEvent) {
         JFXButton btn = (JFXButton) actionEvent.getSource();
         switch (btn.getId()) {
             case "btnSubmit":
-                SceneSwitcher.switchToTemp(getClass(),"/edu/wpi/teamB/views/requestForms/formSubmitted.fxml");
-                break;
+
             case "btnCancel":
                 SceneSwitcher.goBack(getClass(), 1);
                 break;
             case "btnHelp":
                 break;
         }
+    }
+
+    @FXML
+    private void validateButton(){
+        btnSubmit.setDisable(
+                wing.getText().isEmpty() || roomNum.getText().isEmpty() ||
+                !(medicalEmergency.isSelected() || securityEmergency.isSelected()) ||
+                description.getText().isEmpty()
+        );
     }
 }
