474729a33ba164c6a214e5ab92d9f534aa533599
Timothy Goon <t.kgoonx@gmail.com>
2021-04-30 12:58:01 -0400

    Edge is bisected with a node is placed on it.

--- src/main/java/edu/wpi/cs3733/D21/teamB/entities/map/edge/AddNodeFromEdgeWindow.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/entities/map/edge/AddNodeFromEdgeWindow.java
@@ -2,7 +2,9 @@ package edu.wpi.cs3733.D21.teamB.entities.map.edge;
 
 import edu.wpi.cs3733.D21.teamB.database.DatabaseHandler;
 import edu.wpi.cs3733.D21.teamB.entities.map.data.DelEdgePopupData;
+import edu.wpi.cs3733.D21.teamB.entities.map.data.Edge;
 import edu.wpi.cs3733.D21.teamB.entities.map.data.Node;
+import edu.wpi.cs3733.D21.teamB.pathfinding.Graph;
 import edu.wpi.cs3733.D21.teamB.util.Popup.Window;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
@@ -31,11 +33,41 @@ public class AddNodeFromEdgeWindow extends Window<VBox, DelEdgePopupData, VBox> 
      */
     public void addNode(String nodeID, int x, int y, String floor, String building, String type, String longName, String shortName){
 
+        // Delete old edge
+        try {
+            DatabaseHandler.getHandler().removeEdge(
+                    data.getStart().getNodeID() + "_" +
+                            data.getEnd().getNodeID()
+            );
+        } catch (SQLException e) { e.printStackTrace(); }
+
+        // Make the new node
         Node aNode = new Node(nodeID, x, y, floor, building, type, longName, shortName);
 
         try {
             DatabaseHandler.getHandler().addNode(aNode);
         } catch (SQLException e) { e.printStackTrace(); }
+
+        // Make 2 new edges
+        try {
+            if (DatabaseHandler.getHandler().getEdges().get(data.getStart().getNodeID() + "_" +
+                    nodeID) == null)
+                DatabaseHandler.getHandler().addEdge(new Edge(
+                        data.getStart().getNodeID() + "_" +
+                                nodeID, data.getStart().getNodeID(), nodeID
+                ));
+
+            if (DatabaseHandler.getHandler().getEdges().get(nodeID + "_" +
+                    data.getEnd().getNodeID()) == null)
+                DatabaseHandler.getHandler().addEdge(new Edge(
+                        nodeID + "_" +
+                                data.getEnd().getNodeID(), nodeID, data.getEnd().getNodeID()
+                ));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Graph.getGraph().updateGraph();
 
         // Refresh map editor
         data.getMd().refreshEditor();
