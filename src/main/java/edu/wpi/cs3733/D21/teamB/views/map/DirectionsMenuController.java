aa11ebf69d0beb2be7a8c35e10555d220e454dca
Alex Bolduc <aebolduc@wpi.edu>
2021-04-29 20:16:16 -0400

    Test path implemented on google map

--- src/main/java/edu/wpi/cs3733/D21/teamB/views/map/DirectionsMenuController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/views/map/DirectionsMenuController.java
@@ -2,10 +2,12 @@ package edu.wpi.cs3733.D21.teamB.views.map;
 
 import com.dlsc.gmapsfx.GoogleMapView;
 import com.dlsc.gmapsfx.MapComponentInitializedListener;
-import com.dlsc.gmapsfx.javascript.object.GoogleMap;
-import com.dlsc.gmapsfx.javascript.object.LatLong;
-import com.dlsc.gmapsfx.javascript.object.MapOptions;
-import com.dlsc.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.dlsc.gmapsfx.javascript.object.*;
+import com.dlsc.gmapsfx.service.directions.*;
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.D21.teamB.App;
+import edu.wpi.cs3733.D21.teamB.views.BasePageController;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,7 +16,7 @@ import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class DirectionsMenuController implements Initializable, MapComponentInitializedListener {
+public class DirectionsMenuController extends BasePageController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
 
     @FXML
     private AnchorPane anchorPane;
@@ -24,35 +26,63 @@ public class DirectionsMenuController implements Initializable, MapComponentInit
 
     private GoogleMap map;
 
+    DirectionsService directionsService;
+    DirectionsPane directionsPane;
+    DirectionsRenderer directionsRenderer;
+
     @Override
     public void mapInitialized() {
         MapOptions mapOptions = new MapOptions();
 
         mapOptions.center(new LatLong(42.2743, -71.8081))
-                .mapType(MapTypeIdEnum.HYBRID)
+                .mapType(MapTypeIdEnum.ROADMAP)
                 .panControl(true)
-                .rotateControl(false)
-                .streetViewControl(false)
-                .scaleControl(true)
-                .zoom(12);
+                .zoom(12)
+                .overviewMapControl(false);
 
         map = mapView.createMap(mapOptions);
+        directionsService = new DirectionsService();
+        directionsPane = mapView.getDirec();
+
+        map.showDirectionsPane();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mapView = new GoogleMapView("en", "AIzaSyD4MPvha5ZUWDmOuuvxPSMW0NH1h3bZUXs");
         anchorPane.getChildren().add(mapView);
-
         AnchorPane.setTopAnchor(mapView, 0.0);
         AnchorPane.setBottomAnchor(mapView, 0.0);
         AnchorPane.setLeftAnchor(mapView, 0.0);
         AnchorPane.setRightAnchor(mapView, 0.0);
 
-
         mapView.addMapInitializedListener(this);
+
+    }
+
+    private void getRoute(){
+        DirectionsRequest request = new DirectionsRequest("Boston", "Worcester", TravelModes.DRIVING, true);
+        directionsRenderer = new DirectionsRenderer(true, mapView.getMap(), directionsPane);
+        directionsService.getRoute(request, this, directionsRenderer);
     }
 
     public void handleButtonAction(ActionEvent actionEvent) {
+        JFXButton b = (JFXButton) actionEvent.getSource();
+
+        switch (b.getId()){
+            case "btnEmergency":
+                getRoute();
+                break;
+            case "btnExit":
+                Platform.exit();
+                break;
+        }
+    }
+
+    @Override
+    public void directionsReceived(DirectionsResult results, DirectionStatus status) {
+
+        System.out.println("Direction time");
+
     }
 }
