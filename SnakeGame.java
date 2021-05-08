449be3104712d395120242eb1f8f0f3321648a0b
meganletendre7 <65192801+meganletendre7@users.noreply.github.com>
2021-05-08 13:08:04 -0400

    Added the 3 classes needed for snake - does not yet interact with the code just want to have the classes to apply with our applciation

---
+++ SnakeGame.java
@@ -0,0 +1,9 @@
+public class SnakeGame {
+    //reference for code: https://www.youtube.com/watch?v=bI6e6qjJ8JQ
+
+    public static void main(String[] args){
+
+        new gameFrame();
+
+    }
+}
