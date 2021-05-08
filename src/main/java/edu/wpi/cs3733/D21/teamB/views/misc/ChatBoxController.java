484204af06acdbb84c69b35294c584a34645feeb
Timothy Goon <t.kgoonx@gmail.com>
2021-05-08 01:40:16 -0400

    Can talk to chatbot. output is a little buggy tho

--- src/main/java/edu/wpi/cs3733/D21/teamB/views/misc/ChatBoxController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/views/misc/ChatBoxController.java
@@ -47,52 +47,28 @@ public class ChatBoxController implements Initializable {
     private final TextToSpeech tts = new TextToSpeech();
     public static Thread userThread;
 
+    private Chat chatSession;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {
 
-            String resourcesPath = getResourcesPath();
-            System.out.println(resourcesPath);
-            MagicBooleans.trace_mode = false;
-            Bot bot = new Bot("super", resourcesPath);
-            Chat chatSession = new Chat(bot);
-            bot.brain.nodeStats();
-            String textLine = "";
-
-            while(true) {
-                System.out.print("Human : ");
-                textLine = IOUtils.readInputTextLine();
-                if ((textLine == null) || (textLine.length() < 1))
-                    textLine = MagicStrings.null_input;
-                if (textLine.equals("q")) {
-                    System.exit(0);
-                } else if (textLine.equals("wq")) {
-                    bot.writeQuit();
-                    System.exit(0);
-                } else {
-                    String request = textLine;
-                    if (MagicBooleans.trace_mode)
-                        System.out.println("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
-                    String response = chatSession.multisentenceRespond(request);
-                    while (response.contains("<"))
-                        response = response.replace("<", "<");
-                    while (response.contains(">"))
-                        response = response.replace(">", ">");
-                    System.out.println("Robot : " + response);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String resourcesPath = new File(".").getAbsolutePath();
+        System.out.println(resourcesPath);
+        MagicBooleans.trace_mode = false;
+        Bot bot = new Bot("super", resourcesPath);
+        chatSession = new Chat(bot);
+        bot.brain.nodeStats();
 
         // Thread for getting messages from the bot
         userThread = new Thread(() -> {
+
             while (true) {
                 // Wait for a new message
                 if (PageCache.getNewMessagesWaitingForUser().get() != 0) {
                     // Get message and mark as read
                     PageCache.getNewMessagesWaitingForUser().getAndDecrement();
-                    Platform.runLater(() -> sendMessage(PageCache.getBotLastMessage()));
+                    String response = chatSession.multisentenceRespond(PageCache.getBotLastMessage().getMessage());
+                    Platform.runLater(() -> sendMessage(new Message(response, false)));
                 }
             }
         });
@@ -111,17 +87,6 @@ public class ChatBoxController implements Initializable {
             PageCache.getUserMessages().clear();
             PageCache.getBotMessages().clear();
         });
-    }
-
-    private String getResourcesPath() {
-        File currDir = new File(".");
-        String path = currDir.getAbsolutePath();
-        path = path.substring(0, path.length() - 2);
-        System.out.println(path);
-        String resourcesPath = getClass().getResource("/edu/wpi/cs3733/D21/teamB/chat").getPath();;
-
-        resourcesPath = resourcesPath.replaceAll("\\\\", "/");
-        return resourcesPath.substring(1);
     }
 
     @FXML
