9ab652a384822017eba586bd258fde7ed09a7c79
smeragora <smeragora@yahoo.com>
2021-05-07 20:53:49 -0400

    wrote checkApple, changed place apple to have coord data structure.

--- src/main/java/edu/wpi/cs3733/D21/teamB/games/snake/Snake.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/games/snake/Snake.java
@@ -1,6 +1,7 @@
-package edu.wpi.cs3733.D21.teamB.games;
+package edu.wpi.cs3733.D21.teamB.games.snake;
 
 import edu.wpi.cs3733.D21.teamB.database.DatabaseHandler;
+import edu.wpi.cs3733.D21.teamB.entities.map.Coord;
 import edu.wpi.cs3733.D21.teamB.entities.map.data.Edge;
 import edu.wpi.cs3733.D21.teamB.entities.map.data.Node;
 import edu.wpi.cs3733.D21.teamB.util.CSVHandler;
@@ -10,12 +11,13 @@ import java.util.List;
 
 public class Snake {
 
-    private List<Node> nodes;
-    private List<Edge> edges;
-    private int snakeSize;
-    private Node appleLocation;
+    private static List<Node> nodes;
+    private static List<Edge> edges;
+    private static int snakeSize;
+    private static Coord snakeHeadLoc;
+    private static List<Coord> appleLocationList;
 
-    public void initializeMap() {
+    public static void initializeMap() {
         // Load nodes and edges for game
         nodes = CSVHandler.loadCSVNodes("/edu/wpi/cs3733/D21/teamB/csvFiles/snakeNodes.csv");
         edges = CSVHandler.loadCSVEdges("/edu/wpi/cs3733/D21/teamB/csvFiles/snakeEdges.csv");
@@ -26,7 +28,7 @@ public class Snake {
         }
     }
 
-    public void initializeGame() {
+    public static void initializeGame() {
         // set the snake at a starting location
         Node start = nodes.get(0);
         // restart size of snake
@@ -34,14 +36,22 @@ public class Snake {
         // call placeApple()
     }
 
-    public void placeApple() {
+    public static void placeApple() {
         // Randomly select a node to place the apple at
         int index = (int) (Math.random() * nodes.size());
-        Node apple = nodes.get(index);
+        Node appleNode = nodes.get(index);
+        Coord appleCoord = new Coord(appleNode.getXCoord(), appleNode.getYCoord());
+        appleLocationList.add(appleCoord);
     }
 
-    public void checkApple() {
+    public static void checkApple() {
         // Check if the current node location is the location of the apple
         // Add to length of snake and call placeApple()
+        if(appleLocationList.contains(snakeHeadLoc)){
+            //delete the apple off of the map
+            appleLocationList.remove(snakeHeadLoc);
+            snakeSize++;
+        }
+        placeApple();
     }
 }
