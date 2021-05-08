5d763d6b1755c86f5af0847255f6f7859d603a2f
meganletendre7 <65192801+meganletendre7@users.noreply.github.com>
2021-05-08 13:13:22 -0400

    Delete SnakeGame.java

--- SnakeGame.java
+++
@@ -1,9 +0,0 @@
-public class SnakeGame {
-    //reference for code: https://www.youtube.com/watch?v=bI6e6qjJ8JQ
-
-    public static void main(String[] args){
-
-        new gameFrame();
-
-    }
-}
