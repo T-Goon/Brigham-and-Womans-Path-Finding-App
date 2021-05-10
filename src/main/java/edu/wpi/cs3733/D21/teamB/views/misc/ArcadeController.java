065e847b9953bf93583390b7da510f80d6d8b023
smeragora <smeragora@yahoo.com>
2021-05-09 23:34:24 -0400

    arena controller path fixed

--- src/main/java/edu/wpi/cs3733/D21/teamB/views/misc/ArcadeController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/views/misc/ArcadeController.java
@@ -5,9 +5,6 @@ import edu.wpi.cs3733.D21.teamB.views.BasePageController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class ArcadeController extends BasePageController implements Initializable {
 
     @FXML
