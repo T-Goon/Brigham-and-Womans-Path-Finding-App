d1a6876acc7e97d63cc837c95e6505ee9077d7c2
smeragora <smeragora@yahoo.com>
2021-05-10 13:59:41 -0400

    working on putting creds on the same fxml to avoid shifting

--- src/main/java/edu/wpi/cs3733/D21/teamB/views/menus/ServiceRequestMenuController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/views/menus/ServiceRequestMenuController.java
@@ -6,7 +6,11 @@ import edu.wpi.cs3733.D21.teamB.views.BasePageController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServiceRequestMenuController extends BasePageController {
 
@@ -57,6 +61,78 @@ public class ServiceRequestMenuController extends BasePageController {
     @FXML
     private JFXButton btnLanguageInterpreter;
 
+    @FXML
+    private Label lblSocialWorker;
+
+    @FXML
+    private Label lblLaundry;
+
+    @FXML
+    private Label lblFoodDev;
+
+    @FXML
+    private Label lblSan;
+
+    @FXML
+    private Label lblFloral;
+
+    @FXML
+    private Label lblMed;
+
+    @FXML
+    private Label lblSecurity;
+
+    @FXML
+    private Label lblIntTran;
+
+    @FXML
+    private Label lblExtTrans;
+
+    @FXML
+    private Label lblRegReq;
+
+    @FXML
+    private Label lblCase;
+
+    @FXML
+    private Label lblGift;
+
+    @FXML
+    private Label lblLang;
+
+    private List<Label> creds = new ArrayList<>();
+
+    public void hide(){
+        lblSocialWorker.setVisible(false);
+        lblLaundry.setVisible(false);
+        lblFoodDev.setVisible(false);
+        lblSan.setVisible(false);
+        lblFloral.setVisible(false);
+        lblMed.setVisible(false);
+        lblSecurity.setVisible(false);
+        lblIntTran.setVisible(false);
+        lblExtTrans.setVisible(false);
+        lblRegReq.setVisible(false);
+        lblCase.setVisible(false);
+        lblGift.setVisible(false);
+        lblLang.setVisible(false);
+    }
+
+    public void show(){
+        lblSocialWorker.setVisible(true);
+        lblLaundry.setVisible(true);
+        lblFoodDev.setVisible(true);
+        lblSan.setVisible(true);
+        lblFloral.setVisible(true);
+        lblMed.setVisible(true);
+        lblSecurity.setVisible(true);
+        lblIntTran.setVisible(true);
+        lblExtTrans.setVisible(true);
+        lblRegReq.setVisible(true);
+        lblCase.setVisible(true);
+        lblGift.setVisible(true);
+        lblLang.setVisible(true);
+    }
 
     @FXML
     public void handleButtonAction(ActionEvent e) {
@@ -108,6 +184,7 @@ public class ServiceRequestMenuController extends BasePageController {
                 path = VIEWS_PATH + "languageRequestForm.fxml";
                 break;
             case "btnShowCredits":
+                
                 path = "/edu/wpi/cs3733/D21/teamB/views/menus/serviceRequestMenuCredits.fxml";
                 break;
         }
