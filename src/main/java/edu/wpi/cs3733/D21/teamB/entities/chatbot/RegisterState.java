eb3b278ad3d5e233738fc99b676e303f4145ebc8
brian <bfrancis@wpi.edu>
2021-05-11 12:45:07 -0400

    Merge states

--- src/main/java/edu/wpi/cs3733/D21/teamB/entities/chatbot/RegisterState.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/entities/chatbot/RegisterState.java
@@ -14,50 +14,6 @@ public class RegisterState implements IState {
     public List<String> respond(String input) {
         List<String> response = new ArrayList<>();
 
-        if (StateManager.containsAny(input, new String[]{"face", "ID", "faceID"})) {
-
-        } else if (messagesSent.isEmpty()) { // First message
-            response.add("Do you want to make a new account?");
-        } else if (messagesSent.peek().equals("Do you want to make a new account?")) { // Second message
-            if (StateManager.containsAny(input, new String[]{"y", "ye", "yes", "yeah", "yup"})) {
-                response.add("Okay! Taking you there now...");
-                response.add("/edu/wpi/cs3733/D21/teamB/views/login/registerPage.fxml");
-                response.add("Do you need any assistance?");
-                PageCache.getCachedResponses().add("Do you need any assistance?");
-            } else {
-                response.add("No worries!");
-                response.add("return");
-                PageCache.getCachedResponses().add("Is there anything I can help you with?");
-            }
-        } else if (messagesSent.peek().equals("Do you need any assistance?")) { // Third message in
-            if (StateManager.containsAny(input, new String[]{"y", "ye", "yes", "yeah", "yup"})) {
-                response.add("Please fill in all the information in each field.");
-                response.add("You must also take a picture of your face using the 'Take Picture' button so Face ID can work.");
-                response.add("If you have any questions, feel free to ask!");
-            } else if (StateManager.containsAny(input, new String[]{"n", "no", "nah", "nope",})) {
-                response.add("No worries! I'll be here.");
-            } else {
-                response.add("I'm sorry, I didn't understand that.");
-                response.add("Do you need any assistance?");
-            }
-        } else if (messagesSent.peek().equals("No worries! I'll be here.") || messagesSent.peek().equals("If you have any questions, feel free to ask!")) {
-            response.add("Do you need any assistance?");
-        } else { // Otherwise, what the heck?
-            response.add("unsure");
-        }
-
-        for (String s : response)
-            messagesSent.push(s);
-
-        if (PageCache.getCachedResponses() != null && !PageCache.getCachedResponses().isEmpty()) {
-            for (String s : PageCache.getCachedResponses()) {
-                messagesSent.push(s);
-            }
-        }
-
-        return response;
-        List<String> response = new ArrayList<>();
-
         if (!messagesSent.isEmpty() && messagesSent.peek().equals("unsure")) {
             messagesSent.pop();
         }
