ded6210c179453fedf378b6227fad778b4f21c05
brian <bfrancis@wpi.edu>
2021-05-11 13:59:52 -0400

    Expanding COVID state

--- src/main/java/edu/wpi/cs3733/D21/teamB/entities/chatbot/CovidState.java
+++ src/main/java/edu/wpi/cs3733/D21/teamB/entities/chatbot/CovidState.java
@@ -29,10 +29,22 @@ public class CovidState implements IState {
             }
         } else if (messagesSent.peek().equals("Do you need any assistance?")) { // Third message in
             if (StateManager.containsAny(input, new String[]{"y", "ye", "yes", "yeah", "yup"})) {
-                response.add("Please fill out the following questions regarding any symptoms you might be experiencing");
+                response.add("Please fill out the above questions regarding any symptoms you might be experiencing");
                 response.add("If you have any questions, feel free to ask!");
             } else if (StateManager.containsAny(input, new String[]{"n", "no", "nah", "nope",})) {
                 response.add("No worries! I'll be here.");
+            } else if (StateManager.containsAny(input, new String[]{"current","location"})) {
+                response.add("In the current location field please input the closest location to where you currently are");
+                response.add("If you have any questions, feel free to ask!");
+            } else if (StateManager.containsAny(input, new String[]{"symptoms","chills","cough","fever","shortness","sore","headache","ache","runny","nausea","smell"})) {
+                response.add("If you think you might have any of the symptoms above please check as many as you match");
+                response.add("If you have any questions, feel free to ask!");
+            } else if (StateManager.containsAny(input, new String[]{"close","contact"})) {
+                response.add("If you have been around someone diagnosed with COVID in the past two weeks please select yes, otherwise select no");
+                response.add("If you have any questions, feel free to ask!");
+            } else if (StateManager.containsAny(input, new String[]{"positive","test"})) {
+                response.add("If you have received a positive COVID test in the past two weeks please select yes, otherwise select no");
+                response.add("If you have any questions, feel free to ask!");
             } else {
                 response.add("I'm sorry, I didn't understand that.");
                 response.add("Do you need any assistance?");
